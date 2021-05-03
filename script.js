9d59fa0e3ec14e4a2d366ea5042ce118c87510a0
austinjshort <50437454+austinjshort@users.noreply.github.com>
2021-05-02 21:55:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -55,9 +55,4 @@ if (window.location.protocol === 'http:') {
 }
 
 
-function changeBackground(){
-    var url = "url('"
-    var background = document.getElementById("background").value;
-    url = url.concat(background).concat("')")
-    document.getElementById("body").style.backgroundImage = url;
-}
+
