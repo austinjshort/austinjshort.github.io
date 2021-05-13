25fcd321438841172824eed223133edb1f64febd
austinjshort <50437454+austinjshort@users.noreply.github.com>
2021-05-13 01:40:13 -0700

    Update script.js

--- ShortAssignment/script.js
+++ ShortAssignment/script.js
@@ -2,13 +2,13 @@ function changeText(){
     var display = document.getElementById("display").value;
     var stored = localStorage.display;
     if(display == "" && display != stored) {
-        display = localStorage.display;
+        display = localStorage.display
     }
     headID = display.concat("Head");
     textID = display.concat("Body");
     head = document.getElementById(headID).textContent;
     text = document.getElementById(textID).textContent;
-    document.getElementById("displayHead").innerHTML =  head;
+    document.getElementById("displayHead").innerHTML = head;
     document.getElementById("displayText").innerHTML = text;
     //store the value
     localStorage.display = display;
