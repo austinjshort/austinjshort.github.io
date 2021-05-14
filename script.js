f566561516b5da64cbf9b4e7706456987db613c3
austinjshort <50437454+austinjshort@users.noreply.github.com>
2021-05-13 21:13:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeText(){
+    var display = document.getElementById("display").value;
+    var stored = localStorage.display;
+    if(display == "" && display != stored) {
+        display = localStorage.display
+    }
+    headID = display.concat("Head");
+    textID = display.concat("Body");
+    head = document.getElementById(headID).textContent;
+    text = document.getElementById(textID).textContent;
+    document.getElementById("displayHead").innerHTML = head;
+    document.getElementById("displayText").innerHTML = text;
+    //store the value
+    localStorage.display = display;
+}
